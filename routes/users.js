3bdbaa80134189f9a7f26f2fb3f329f44263e8af
daniel kacou <danleynewline@gmail.com>
2023-04-22 13:24:43 +0000

    backend obk is finished

--- routes/users.js
+++ routes/users.js
@@ -4,18 +4,12 @@ import { verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-
-//UPDATE
 router.put("/:id", verifyUser,updateUser);
 
-//DELETE
 router.delete("/:id",verifyUser, deleteUser);
 
-//GET 
 router.get("/:id", verifyUser, getUser);
 
-
-//GET ALL
 router.get("/", verifyUser, getUsers);
 
 export default router;
