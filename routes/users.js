fe059de03c22f80e3bf529cd740b23fffae2c1bc
daniel kacou <danleynewline@gmail.com>
2023-04-22 13:23:50 +0000

    backend obk is finished

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,9 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/User.js";
-import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+import { verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-// router.get("/checkauthentication", verifyToken, (req, res, next)=>{
-//     res.send("Hello user your are logged!")
-// })
-
-// router.get("/checkuser/:id", verifyUser, (req, res, next)=>{
-//     res.send("Hello user your are logged and you can delete your account!")
-// })
-
-// router.get("/checkadmin/:id", verifyAdmin, (req, res, next)=>{
-//     res.send("Hello Admin your are logged and you can delete all account!")
-// })
 
 //UPDATE
 router.put("/:id", verifyUser,updateUser);
@@ -27,6 +16,6 @@ router.get("/:id", verifyUser, getUser);
 
 
 //GET ALL
-router.get("/", verifyAdmin, getUsers);
+router.get("/", verifyUser, getUsers);
 
 export default router;
